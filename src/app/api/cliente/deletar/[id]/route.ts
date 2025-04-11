682999ebbcdaedea10a790d723787608684e5794
roberto <xdstuart@gmail.com>
2025-04-11 20:32:06 -0300

    tes

--- src/app/api/cliente/deletar/[id]/route.ts
+++ src/app/api/cliente/deletar/[id]/route.ts
@@ -3,7 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 const MASTER_KEY = process.env.MASTER_KEY || "zapchatbr.com";
 const BASE_URL = "https://panel.zapchatbr.com";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
   const id = context.params?.id;
   const url = new URL(req.url);
