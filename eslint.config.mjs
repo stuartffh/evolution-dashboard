4f7cc42a8a0bec82e16fd7fe22bb2b0b179dcc2a
roberto <xdstuart@gmail.com>
2025-04-11 20:28:52 -0300

    esc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,15 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
-    rules: {
-      // Regras que você quer desabilitar ou ajustar:
-      "@typescript-eslint/no-explicit-any": "off",
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-page-custom-font": "off",
-    },
-  }),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
 export default eslintConfig;
